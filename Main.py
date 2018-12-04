57a45a6febf4454778f9089318d8e7ca04bb9f57
patilana22 <44269037+patilana22@users.noreply.github.com>
2018-12-04 12:34:07 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,105 @@
+
+
+
+
+#Code
+
+
+
+import os
+import math
+import time
+import sys
+import random
+
+
+from time import sleep
+import webbrowser
+ear = 0
+url = 'https://corgiorgy.com/'
+url_1 = 'http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html'
+url_2 = "https://www.youtube.com/watch?v=cv6a81Ulx-g"
+def practice():
+    i = 0
+    hi = "Hi Anand......\n"
+    enter = "Please press enter to continue\n"
+    message = "This is a very important message that you are reading!\n"
+    virus = "I am downloading a virus to your computer as we speak!\n"
+    for char in hi:
+        sleep(0.1)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+   
+  
+
+    
+    raw_input("Please press enter to continue")
+
+  
+     
+    for char in message:
+        sleep(0.1)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+
+    raw_input("Please press enter to continue")
+
+    
+  
+    
+
+    for char in virus:
+        sleep(0.1)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+
+
+    raw_input("Please press enter to continue")
+    
+
+
+    print "5"
+
+    time.sleep(1)
+
+    print "4"
+
+    time.sleep(1)
+
+    print "3"
+
+    time.sleep(1)
+
+    print "2"
+
+    time.sleep(1)
+
+    print "1"
+
+    while (True):
+            #chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+            #webbrowser.get(chrome_path).open_new(url)
+            #webbrowser.open_new(chrome_path)
+
+            chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+            webbrowser.get(chrome_path).open(url)
+            webbrowser.get(chrome_path).open(url_1)
+            webbrowser.get(chrome_path).open(url_2)
+            
+        #random.randint(0, 10000000000000000000000000000000000)
+           
+
+practice()
+
+
+
+
+
+
+
+
+
